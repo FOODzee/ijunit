030f17ea8c3c25f2a13fd741a60b5a94abc4c142
foodzee <foodzzzeat.ama@gmail.com>
2016-01-08 22:47:02 +0600

    improve interrupting

--- src/sc/ijunit/core/Main.java
+++ src/sc/ijunit/core/Main.java
@@ -31,16 +31,16 @@ public class Main {
         try {
             numberOfThreads = Integer.decode(args[0]);
         } catch (NumberFormatException e) {
-            System.out.println("First parameter must be an integer specifying number of threads to use.");
+            System.err.println("First parameter must be an integer specifying number of threads to use.");
             usage();
         }
 
         if (numberOfThreads == 0) {
-            System.out.println("You specified zero threads to use, that makes no sense.");
+            System.err.println("You specified zero threads to use, that makes no sense.");
             usage();
         } else if (numberOfThreads > Runtime.getRuntime().availableProcessors()) {
-            System.out.println("You specified more threads to use than it is available.");
-            System.out.println("That makes no sense, but is not critical.");
+            System.err.println("You specified more threads to use than it is available.");
+            System.err.println("That makes no sense, but is not critical.");
         }
 
         Tester[] testers = new Tester[numberOfThreads];
@@ -53,38 +53,46 @@ public class Main {
             try {
                 jobs.add(Class.forName(args[i]));
             } catch (ClassNotFoundException e) {
-                System.out.println(e + " at " + i + " argument");
-                interruptAndWait(testers);
+                System.err.println(e + " at " + i + " argument");
+                softlyInterrupt(testers);
                 usage();
             }
         }
 
-        while (!jobs.isEmpty()) synchronized (Thread.currentThread()) {
-            try   { Thread.currentThread().wait(10); }
-            catch ( InterruptedException e )
-            { /* Give testers some time to check remaining tests. */ }
-        }
-
-        interruptAndWait(testers);
+        softlyInterrupt(testers);
     }
 
-    protected static void interruptAndWait(Tester[] testers) {
-        for (Tester t : testers) t.interrupt();
-
-        boolean aliveTesters = true;
-        while (aliveTesters) synchronized (Thread.currentThread()) {
-            aliveTesters = false;
-            for (Tester t : testers) {
-                if (t.isAlive()) {
-                    aliveTesters = true;
-                    break;
-                }
+    /**
+     * Wait while testers finish all jobs in the list
+     * then interrupt them, but wait for last job to be finished.
+     */
+    protected static void softlyInterrupt(Tester[] testers) {
+        try { synchronized (Thread.currentThread()) {
+            while (!jobs.isEmpty()) {
+                // Give testers some time to check remaining tests.
+                Thread.currentThread().wait(10);
             }
-        }
+
+            for (Tester t : testers) t.interrupt();
+
+            boolean aliveTesters;
+            do {
+                aliveTesters = false;
+                for (Tester t : testers) {
+                    if (t.isAlive()) {
+                        aliveTesters = true;
+                        break;
+                    }
+                }
+                // Give alive testers some time to finish their last test.
+                Thread.currentThread().wait(10);
+            } while (aliveTesters);
+        }} catch (InterruptedException e) { /* Our waits won't be interrupted. */ }
     }
 
     private static void usage() {
-        System.out.printf("Usage:\n\tjava -cp ijunit.jar;<tested-classes> sc.ijunit.core.Main N class-name [class-name]*\n");
+        System.err.printf("Usage:\n" +
+                "\tjava -cp ijunit.jar;<tested-classes> sc.ijunit.core.Main N class-name [class-name]*\n");
         System.exit(-1);
     }
 }
